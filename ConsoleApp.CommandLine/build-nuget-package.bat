5c57201c5a3820bf1133a385fa526e1c2ddaebe7
shai_hulud <deniszykov@gmail.com>
2016-02-23 21:59:10 +0300

    updated build-nuget-package.bat

--- ConsoleApp.CommandLine/build-nuget-package.bat
+++ ConsoleApp.CommandLine/build-nuget-package.bat
@@ -1,1 +1,3 @@
-nuget pack ConsoleApp.CommandLine.csproj -Prop Configuration=Release -Prop Platform=AnyCPU
+SET OUTPUT_DIR=%1
+IF "%OUTPUT_DIR%"=="" SET OUTPUT_DIR=%CD%
+nuget pack ConsoleApp.CommandLine.csproj -Prop Configuration=Release -Prop Platform=AnyCPU -OutputDirectory %OUTPUT_DIR%
