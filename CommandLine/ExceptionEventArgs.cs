1a338850da1a1f8c7aa335322ad5a7e2a7597e39
Denis Zykov <deniszykov@gmail.com>
2020-09-26 14:30:08 +0300

    chore: copyright update

--- CommandLine/ExceptionEventArgs.cs
+++ CommandLine/ExceptionEventArgs.cs
@@ -1,9 +1,9 @@
 ﻿/*
-	Copyright (c) 2017 Denis Zykov
+	Copyright (c) 2020 Denis Zykov
 	
 	This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 
 	License: https://opensource.org/licenses/MIT
 */
